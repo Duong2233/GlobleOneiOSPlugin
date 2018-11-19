11d1a007ddea12664da4970ada628dbeafc37086
Duong Nguyen <duong.nh@sutruxsolution.com>
2018-11-19 11:49:43 +0700

    Initial Commit

---
+++ GlobleOneIOSPlugin/GlobleOneIOSPlugin.h
@@ -0,0 +1,19 @@
+//
+//  GlobleOneIOSPlugin.h
+//  GlobleOneIOSPlugin
+//
+//  Created by dev on 11/19/18.
+//  Copyright © 2018 GlobleOneIOSPlugin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GlobleOneIOSPlugin.
+FOUNDATION_EXPORT double GlobleOneIOSPluginVersionNumber;
+
+//! Project version string for GlobleOneIOSPlugin.
+FOUNDATION_EXPORT const unsigned char GlobleOneIOSPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GlobleOneIOSPlugin/PublicHeader.h>
+
+
